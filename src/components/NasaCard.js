9f74195eba6a27eff5d810e800495089ef7cee57
Victor Sanchez <v08311996@gmail.com>
2020-01-10 19:01:47 -0500

    Updated Changes for part 2

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,14 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
-const NasaCard = props => {
+const StyledHeadingOne = styled.h1`
+    background: #d39f16;
+    color: white;
+    width: 40%;
+    margin-left: 30%;
+    margin-top: 1%;
+    padding-top: 8px;
+    padding-bottom: 10px;
+`;
+
+const StyledImage = styled.img`
+    width: 700px;
+    height: 600px;
+    margin-top: 1%;
+    margin-bottom: 1%;
+    border-radius: 5px;
+`;
+
+const StyledHeadingThree = styled.h3`
+  
+`;
+
+const StyledParagraph = styled.p`
+    width: 60%;
+    margin-left: 20%;
+`;
+
+const NasaCard = (props) => {
   return (
-    <div className="file-list">
-      <h2>Nasa Photo Of The Day: {props.title}</h2>
-      <img src={props.hdurl} />
-      <p>{props.explanation}</p>
-      <p>Anything: {props.date}</p>
-      <p>Anything {props.title}</p>
-    </div>
-  );
+      <div className="NasaCard">
+          <StyledHeadingOne>Astronomy Picture of the Day</StyledHeadingOne>
+          <StyledImage src={props.hdurl}></StyledImage>
+          <StyledHeadingThree>Date: {props.date}</StyledHeadingThree>
+          <StyledParagraph>Explanation: {props.explanation}</StyledParagraph>
+      </div>
+  )
 };
+
 export default NasaCard;
+
